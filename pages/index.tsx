56dda738d455796159111268663aac3ba829667a
Pikago <72993364+Pikago-hub@users.noreply.github.com>
2024-01-26 21:34:12 -0600

    fix build issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import {
   Typography,
   Box,
-  Card,
   Container,
-  Button,
   styled
 } from '@mui/material';
 import type { ReactElement } from 'react';
@@ -11,19 +9,18 @@ import BaseLayout from 'src/layouts/BaseLayout';
 
 import Link from 'src/components/Link';
 import Head from 'next/head';
-import Logo from 'src/components/LogoSign';
 import Hero from 'src/content/Overview/Hero';
 import Header from 'src/components/Header'; 
 
-const HeaderWrapper = styled(Card)(
-  ({ theme }) => `
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: ${theme.spacing(10)};
-  margin-bottom: ${theme.spacing(10)};
-`
-);
+// const HeaderWrapper = styled(Card)(
+//   ({ theme }) => `
+//   width: 100%;
+//   display: flex;
+//   align-items: center;
+//   height: ${theme.spacing(10)};
+//   margin-bottom: ${theme.spacing(10)};
+// `
+// );
 
 const OverviewWrapper = styled(Box)(
   ({ theme }) => `
